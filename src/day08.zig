ec45eaddff00a2a2fc977d5249e55ebdff218304
Jack Huff <jgh713@live.com>
2023-12-09 09:01:36 -0600

    Bad zig stack trace example

--- src/day08.zig
+++ src/day08.zig
@@ -9,9 +9,165 @@ const util = @import("util.zig");
 const gpa = util.gpa;
 
 const data = @embedFile("data/day08.txt");
+const testdata = "LLR\n\nAAA = (BBB, BBB)\nBBB = (AAA, ZZZ)\nZZZ = (ZZZ, ZZZ)";
+
+test "day8_part1" {
+    const res = part1(testdata);
+    assert(res == 6);
+}
+
+fn part1(input: []const u8) u32 {
+    var step: u16 = 0;
+    var steps: [310]u1 = undefined;
+
+    while (input[step] != '\n') {
+        step += 1;
+    }
+
+    if (input[step - 1] == '\r') {
+        step -= 1;
+    }
+
+    for (input[0..step], 0..) |c, i| {
+        steps[i] = switch (c) {
+            'L' => 0,
+            'R' => 1,
+            else => unreachable,
+        };
+    }
+
+    var current: u16 = 0;
+    var map: [65535][2]u16 = undefined;
+    var this: u16 = 0;
+    var left: u16 = 0;
+
+    for (input[step + 1 ..]) |c| {
+        switch (c) {
+            'A'...'Z' => {
+                current = (current << 5) | (c - 'A');
+            },
+            '(' => {
+                this = current;
+                current = 0;
+            },
+            ',' => {
+                left = current;
+                current = 0;
+            },
+            ')' => {
+                map[this][0] = left;
+                map[this][1] = current;
+                current = 0;
+            },
+            else => continue,
+        }
+    }
+
+    var loc: u16 = 0;
+    var count: u32 = 0;
+    const dest = (25 << 10) | (25 << 5) | 25;
+    while (loc != dest) {
+        loc = map[loc][steps[count % step]];
+        count += 1;
+    }
+
+    return count;
+}
+
+const Loop = struct {
+    offset: u64,
+    length: u64,
+};
+
+inline fn getLoop(start: u16, map: [65535][2]u16, step: u16) Loop {
+    var offset: u64 = 0;
+    var loc: u16 = start;
+    var count: u32 = 0;
+    while (true) {
+        loc = map[loc][(count % step)];
+        count += 1;
+        if (loc & 0b11111 == 25) {
+            offset = count;
+        }
+        if (loc == start) {
+            return Loop{ .offset = offset, .length = count };
+        }
+    }
+}
+
+fn part2(input: []const u8) u32 {
+    var step: u16 = 0;
+    var steps: [310]u1 = undefined;
+
+    while (input[step] != '\n') {
+        step += 1;
+    }
+
+    if (input[step - 1] == '\r') {
+        step -= 1;
+    }
+
+    for (input[0..step], 0..) |c, i| {
+        steps[i] = switch (c) {
+            'L' => 0,
+            'R' => 1,
+            else => unreachable,
+        };
+    }
+
+    var current: u16 = 0;
+    var map: [65535][2]u16 = undefined;
+    var this: u16 = 0;
+    var left: u16 = 0;
+    var node: u4 = 0;
+    var nodes: [10]u16 = undefined;
+
+    for (input[step + 1 ..]) |c| {
+        switch (c) {
+            'A'...'Z' => {
+                current = (current << 5) | (c - 'A');
+            },
+            '(' => {
+                this = current;
+                current = 0;
+            },
+            ',' => {
+                left = current;
+                current = 0;
+            },
+            ')' => {
+                map[this][0] = left;
+                map[this][1] = current;
+                current = 0;
+                if (this & 0b11111 == 0) {
+                    nodes[node] = this;
+                    node += 1;
+                }
+            },
+            else => continue,
+        }
+    }
+
+    var loops: [10]Loop = undefined;
+    for (0..node) |i| {
+        print("Node {}\n", .{i});
+        loops[i] = getLoop(nodes[i], map, step);
+        print("Got loop {} with offset {} and length {}\n", .{ i, loops[i].offset, loops[i].length });
+    }
+
+    return 0;
+}
 
 pub fn main() !void {
-    
+    var timer = try std.time.Timer.start();
+    const res = part1(data);
+    const time1 = timer.lap();
+    const res2 = part2(data);
+    const time2 = timer.lap();
+    print("Part1: {}\n", .{res});
+    print("Part2: {}\n", .{res2});
+    print("Part1 took {}ns\n", .{time1});
+    print("Part2 took {}ns\n", .{time2});
 }
 
 // Useful stdlib functions
--- src/day09.zig
+++ src/day09.zig
@@ -9,9 +9,131 @@ const util = @import("util.zig");
 const gpa = util.gpa;
 
 const data = @embedFile("data/day09.txt");
+const testdata = "0 3 6 9 12 15\n1 3 6 10 15 21\n10 13 16 21 30 45\n";
+
+test "day9_part1" {
+    const res = part1(testdata, false);
+    assert(res == 114);
+}
+
+inline fn allZeroes(nums: []const i64, count: u8) bool {
+    for (nums[0..count]) |num| {
+        if (num != 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+fn getLineVal(nums: []const i64, count: u8) i128 {
+    var newnums: [50]i64 = undefined;
+
+    if (allZeroes(nums, count)) {
+        return 0;
+    }
+
+    for (0..(count - 1)) |i| {
+        newnums[i] = nums[i + 1] - nums[i];
+    }
+
+    const diff = getLineVal(&newnums, count - 1);
+
+    return diff + nums[count - 1];
+}
+
+fn part1(input: []const u8, isfile: bool) i128 {
+    var itline: std.mem.SplitIterator(u8, .sequence) = undefined;
+    if (isfile) {
+        itline = splitSeq(u8, input, "\r\n");
+    } else {
+        itline = splitSeq(u8, input, "\n");
+    }
+
+    var total: i128 = 0;
+
+    while (itline.next()) |line| {
+        var itnum = splitSeq(u8, line, " ");
+        var nums: [50]i64 = undefined;
+        var i: u8 = 0;
+        while (itnum.next()) |num| {
+            if (num.len == 0) {
+                continue;
+            }
+            nums[i] = parseInt(i64, num, 10) catch {
+                print("Failed to parse number: {s}\n", .{num});
+                unreachable;
+            };
+            i += 1;
+        }
+
+        total += getLineVal(&nums, i);
+    }
+
+    return total;
+}
+
+test "day9_part2" {
+    const res = part2(testdata, false);
+    assert(res == 2);
+}
+
+fn getLineValLeft(nums: []const i64, count: u8) i128 {
+    var newnums: [50]i64 = undefined;
+
+    if (allZeroes(nums, count)) {
+        return 0;
+    }
+
+    for (0..(count - 1)) |i| {
+        newnums[i] = nums[i + 1] - nums[i];
+    }
+
+    const diff = getLineValLeft(&newnums, count - 1);
+
+    return nums[0] - diff;
+}
+
+fn part2(input: []const u8, isfile: bool) i128 {
+    var itline: std.mem.SplitIterator(u8, .sequence) = undefined;
+    if (isfile) {
+        itline = splitSeq(u8, input, "\r\n");
+    } else {
+        itline = splitSeq(u8, input, "\n");
+    }
+
+    var total: i128 = 0;
+
+    while (itline.next()) |line| {
+        var itnum = splitSeq(u8, line, " ");
+        var nums: [50]i64 = undefined;
+        var i: u8 = 0;
+        while (itnum.next()) |num| {
+            if (num.len == 0) {
+                continue;
+            }
+            nums[i] = parseInt(i64, num, 10) catch {
+                print("Failed to parse number: {s}\n", .{num});
+                unreachable;
+            };
+            i += 1;
+        }
+
+        total += getLineValLeft(&nums, i);
+    }
+
+    return total;
+}
 
 pub fn main() !void {
-    
+    var timer = try std.time.Timer.start();
+    const res = part1(data, true);
+    const time1 = timer.lap();
+    const res2 = part2(data, true);
+    const time2 = timer.lap();
+    print("Part1: {}\n", .{res});
+    print("Part2: {}\n", .{res2});
+    print("Part1 took {}ns\n", .{time1});
+    print("Part2 took {}ns\n", .{time2});
 }
 
 // Useful stdlib functions
