a9b84c90799acd9325ffee0ff4e29850c41749c5
Jack Huff <jgh713@live.com>
2023-12-28 20:26:42 -0600

    Days 18 and 19

--- src/day18.zig
+++ src/day18.zig
@@ -9,9 +9,368 @@ const util = @import("util.zig");
 const gpa = util.gpa;
 
 const data = @embedFile("data/day18.txt");
+const testdata = "R 6 (#70c710)\nD 5 (#0dc571)\nL 2 (#5713f0)\nD 2 (#d2c081)\nR 2 (#59c680)\nD 2 (#411b91)\nL 5 (#8ceee2)\nU 2 (#caa173)\nL 1 (#1b58a2)\nU 2 (#caa171)\nR 2 (#7807d2)\nU 3 (#a77fa3)\nL 2 (#015232)\nU 2 (#7a21e3)";
+
+test "day18_part1" {
+    const res = part1(testdata);
+    assert(res == 62);
+}
+
+const Tile = struct {
+    color: u24 = 0,
+    interior: ?bool = null,
+    queued: bool = false,
+
+    fn init() Tile {
+        return .{ .color = 0, .interior = null, .queued = false };
+    }
+};
+const mapwidth = 500;
+const Tilemap = [mapwidth][mapwidth]Tile;
+
+inline fn neighbors(x: u16, y: u16) [4]?[2]u16 {
+    var res: [4]?[2]u16 = std.mem.zeroes([4]?[2]u16);
+    var count: u8 = 0;
+    if (x > 0) {
+        res[count] = .{ x - 1, y };
+        count += 1;
+    }
+    if (x < mapwidth - 1) {
+        res[count] = .{ x + 1, y };
+        count += 1;
+    }
+    if (y > 0) {
+        res[count] = .{ x, y - 1 };
+        count += 1;
+    }
+    if (y < mapwidth - 1) {
+        res[count] = .{ x, y + 1 };
+        count += 1;
+    }
+    return res;
+}
+
+fn floodFillExterior(map: *Tilemap, ix: u16, iy: u16) void {
+    if (map[iy][ix].interior) |_| return;
+    var queue: [100000][2]u16 = undefined;
+    var qi: u32 = 1;
+
+    queue[0] = .{ ix, iy };
+
+    while (qi > 0) {
+        qi -= 1;
+        const pos = queue[qi];
+        const x = pos[0];
+        const y = pos[1];
+        if (map[y][x].interior) |_| continue;
+        map[y][x].interior = false;
+
+        for (neighbors(x, y)) |np| {
+            if (np) |npc| {
+                const nx = npc[0];
+                const ny = npc[1];
+                if (map[ny][nx].interior == null) {
+                    queue[qi] = .{ nx, ny };
+                    qi += 1;
+                }
+            }
+        }
+    }
+}
+
+fn buildMap(map: *Tilemap, input: []const u8) void {
+    for (map) |*row| {
+        for (row) |*tile| {
+            tile.* = Tile.init();
+        }
+    }
+
+    var x: u16 = 114;
+    var y: u16 = 261;
+    if (mapwidth < 100) {
+        x = 0;
+        y = 0;
+    }
+    var xmod: i2 = 0;
+    var ymod: i2 = 0;
+
+    print("Started build\n", .{});
+
+    var lines = splitSca(u8, input, '\n');
+    while (lines.next()) |line| {
+        var args = splitSca(u8, line, ' ');
+        switch (args.next().?[0]) {
+            'R' => {
+                xmod = 1;
+                ymod = 0;
+            },
+            'L' => {
+                xmod = -1;
+                ymod = 0;
+            },
+            'U' => {
+                xmod = 0;
+                ymod = -1;
+            },
+            'D' => {
+                xmod = 0;
+                ymod = 1;
+            },
+            else => unreachable,
+        }
+
+        const distance = parseInt(u8, args.next().?, 16) catch unreachable;
+        const color = parseInt(u24, args.next().?[2..8], 16) catch unreachable;
+
+        for (0..distance) |_| {
+            x = @intCast(@as(i32, x) + xmod);
+            y = @intCast(@as(i32, y) + ymod);
+            map[@intCast(y)][@intCast(x)] = Tile{ .color = color, .interior = true };
+        }
+    }
+
+    print("Gothere\n", .{});
+
+    for (0..mapwidth) |ix| {
+        var iy: u32 = 0;
+        while (iy < mapwidth and map[iy][ix].interior == null) {
+            map[iy][ix].interior = false;
+            iy += 1;
+        }
+        iy = mapwidth - 1;
+        while (map[iy][ix].interior == null and iy > 0) {
+            map[iy][ix].interior = false;
+            iy -= 1;
+        }
+    }
+
+    for (0..mapwidth) |iy| {
+        var ix: u32 = 0;
+        while (ix < mapwidth and map[iy][ix].interior == null) {
+            map[iy][ix].interior = false;
+            ix += 1;
+        }
+        ix = mapwidth - 1;
+        while (map[iy][ix].interior == null and ix > 0) {
+            map[iy][ix].interior = false;
+            ix -= 1;
+        }
+    }
+
+    for (0..mapwidth) |iy| {
+        for (0..mapwidth) |ix| {
+            if (map[iy][ix].interior == null) {
+                neighbors: for (neighbors(@intCast(ix), @intCast(iy))) |np| {
+                    if (np) |npc| {
+                        const nx = npc[0];
+                        const ny = npc[1];
+                        if (map[ny][nx].interior == false) {
+                            floodFillExterior(map, @intCast(ix), @intCast(iy));
+                            break :neighbors;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+fn printMap(map: *Tilemap) void {
+    for (map) |row| {
+        for (row) |tile| {
+            if (tile.interior == null) {
+                print("?", .{});
+            } else if (tile.interior == false) {
+                print(".", .{});
+            } else {
+                print("#", .{});
+            }
+        }
+        print("\n", .{});
+    }
+}
+
+fn part1(input: []const u8) usize {
+    const sx: i32 = 0;
+    const sy: i32 = 0;
+    var x: i32 = sx;
+    var y: i32 = sy;
+
+    var top: i128 = 0;
+    var bottom: i128 = 0;
+    var perimeter: usize = 0;
+
+    var lines = splitSca(u8, input, '\n');
+    while (lines.next()) |line| {
+        var args = splitSca(u8, line, ' ');
+        const dir: u8 = args.next().?[0];
+
+        const distance = parseInt(u8, args.next().?, 10) catch unreachable;
+        //const color = parseInt(u24, args.next().?[2..8], 16) catch unreachable;
+        //_ = color;
+
+        var nx: i32 = x;
+        var ny: i32 = y;
+
+        switch (dir) {
+            'R' => {
+                nx += distance;
+            },
+            'L' => {
+                nx -= distance;
+            },
+            'U' => {
+                ny -= distance;
+            },
+            'D' => {
+                ny += distance;
+            },
+            else => unreachable,
+        }
+
+        top += @as(i128, x) * ny;
+        bottom += @as(i128, y) * nx;
+        perimeter += distance;
+
+        x = nx;
+        y = ny;
+    }
+
+    if (x != sx or y != sy) {
+        const nx = sx;
+        const ny = sy;
+
+        top += @as(i64, x) * ny;
+        bottom += @as(i64, y) * nx;
+        perimeter += @as(usize, @abs(@as(i64, sx) - @as(i64, x)) + @abs(@as(i64, sy) - @as(i64, y)));
+    }
+
+    const diff = @abs(@as(i256, top) - @as(i256, bottom));
+
+    const res: usize = @intCast((diff + perimeter) / 2 + 1);
+    return res;
+}
+
+test "day18_part2" {
+    const res = part2(testdata);
+    assert(res == 952408144115);
+}
+
+fn part2Slow(input: []const u8) u128 {
+    const sx: i64 = 0;
+    const sy: i64 = 0;
+    var x: i64 = sx;
+    var y: i64 = sy;
+
+    var top: i256 = 0;
+    var bottom: i256 = 0;
+    var perimeter: u128 = 0;
+
+    var lines = splitSca(u8, input, '\n');
+    while (lines.next()) |line| {
+        var args = splitSca(u8, line, ' ');
+        const dir: u8 = args.next().?[0];
+        _ = dir;
+
+        _ = args.next();
+        //const distance = parseInt(u8, args.next().?, 10) catch unreachable;
+        const color = args.next().?[2..8];
+        const distance = parseInt(u20, color[0..5], 16) catch unreachable;
+
+        var nx: i64 = x;
+        var ny: i64 = y;
+
+        switch (color[5]) {
+            '0' => {
+                nx += distance;
+            },
+            '2' => {
+                nx -= distance;
+            },
+            '3' => {
+                ny -= distance;
+            },
+            '1' => {
+                ny += distance;
+            },
+            else => unreachable,
+        }
+
+        top += @as(i128, x) * ny;
+        bottom += @as(i128, y) * nx;
+        perimeter += distance;
+
+        x = nx;
+        y = ny;
+    }
+
+    if (x != sx or y != sy) {
+        const nx = sx;
+        const ny = sy;
+
+        top += @as(i128, x) * ny;
+        bottom += @as(i128, y) * nx;
+        perimeter += @as(u128, @abs(@as(i64, sx) - @as(i64, x)) + @abs(@as(i64, sy) - @as(i64, y)));
+    }
+
+    const diff = @abs(@as(i256, top) - @as(i256, bottom));
+
+    return @intCast((diff + perimeter) / 2 + 1);
+}
+
+fn part2(input: []const u8) u128 {
+    const sx: i128 = 0;
+    const sy: i128 = 0;
+    var x: i128 = sx;
+    var y: i128 = sy;
+    var diff: i128 = 0;
+    var perimeter: u128 = 0;
+
+    for (input, 0..) |c, i| {
+        if (c != '#') continue;
+
+        const color = input[i + 1 .. i + 7];
+        const distance = parseInt(u20, color[0..5], 16) catch unreachable;
+
+        var nx: i128 = x;
+        var ny: i128 = y;
+
+        switch (color[5]) {
+            '0' => {
+                nx += distance;
+            },
+            '2' => {
+                nx -= distance;
+            },
+            '3' => {
+                ny -= distance;
+            },
+            '1' => {
+                ny += distance;
+            },
+            else => unreachable,
+        }
+
+        diff += (x * ny) - (y * nx);
+        perimeter += distance;
+
+        x = nx;
+        y = ny;
+    }
+
+    return @intCast((@abs(diff) + perimeter) / 2 + 1);
+}
 
 pub fn main() !void {
-    
+    var timer = try std.time.Timer.start();
+    const res = part1(data);
+    const time = timer.lap();
+    const res2 = part2(data);
+    const time2 = timer.lap();
+    print("Part 1: {}\n", .{res});
+    print("Part1 took {}ns\n", .{time});
+    print("Part 2: {}\n", .{res2});
+    print("Part2 took {}ns\n", .{time2});
 }
 
 // Useful stdlib functions
--- src/day19.zig
+++ src/day19.zig
@@ -9,9 +9,273 @@ const util = @import("util.zig");
 const gpa = util.gpa;
 
 const data = @embedFile("data/day19.txt");
+const testdata = "px{a<2006:qkq,m>2090:A,rfg}\npv{a>1716:R,A}\nlnx{m>1548:A,A}\nrfg{s<537:gd,x>2440:R,A}\nqs{s>3448:A,lnx}\nqkq{x<1416:A,crn}\ncrn{x>2662:A,R}\nin{s<1351:px,qqz}\nqqz{s>2770:qs,m<1801:hdj,R}\ngd{a>3333:R,R}\nhdj{m>838:A,pv}\n\n{x=787,m=2655,a=1222,s=2876}\n{x=1679,m=44,a=2067,s=496}\n{x=2036,m=264,a=79,s=2244}\n{x=2461,m=1339,a=466,s=291}\n{x=2127,m=1623,a=2188,s=1013}";
+test "day19_part1" {
+    const res = part1(testdata);
+    assert(res == 19114);
+}
+
+const Condition = enum { LT, GT };
+
+const Rule = struct {
+    condition: ?Condition,
+    valueid: u2 = undefined,
+    limit: u16 = undefined,
+    destination: u16,
+};
+
+const Ruleset = [5]Rule;
+
+fn ruleid(str: []const u8) u16 {
+    switch (str[0]) {
+        'A' => return 0,
+        'R' => return 1,
+        else => {
+            var id: u16 = 0;
+            for (str) |c| {
+                id <<= 5;
+                id |= c - 'a';
+            }
+            return id;
+        },
+    }
+}
+
+inline fn valueid(c: u8) u2 {
+    return switch (c) {
+        'x' => 0,
+        'm' => 1,
+        'a' => 2,
+        's' => 3,
+        else => unreachable,
+    };
+}
+
+fn part1(input: []const u8) usize {
+    var lines = splitSca(u8, input, '\n');
+    var rulesets: [65536]Ruleset = undefined;
+
+    while (lines.next()) |line| {
+        if (line.len < 2) break;
+        const start: usize = indexOf(u8, line, '{').?;
+        const end: usize = indexOf(u8, line, '}').?;
+        const rule_id = ruleid(line[0..start]);
+        var ruleit = splitSca(u8, line[start + 1 .. end], ',');
+        var ruleset: Ruleset = undefined;
+        var ri: u3 = 0;
+        while (ruleit.next()) |rulestr| {
+            const rule: Rule = switch (rulestr[0]) {
+                'A' => Rule{ .condition = null, .destination = 0 },
+                'R' => Rule{ .condition = null, .destination = 1 },
+                else => blk: {
+                    const colonpos = indexOf(u8, rulestr, ':');
+                    if (colonpos) |colon| {
+                        const value_id: u2 = valueid(rulestr[0]);
+                        const cond: Condition = switch (rulestr[1]) {
+                            '>' => Condition.GT,
+                            '<' => Condition.LT,
+                            else => unreachable,
+                        };
+                        const limit: u16 = parseInt(u16, rulestr[2..colon], 10) catch unreachable;
+                        const dest = ruleid(rulestr[colon + 1 .. rulestr.len]);
+                        break :blk Rule{ .condition = cond, .valueid = value_id, .limit = limit, .destination = dest };
+                    } else {
+                        const dest = ruleid(rulestr);
+                        break :blk Rule{ .condition = null, .destination = dest };
+                    }
+                },
+            };
+            ruleset[ri] = rule;
+            ri += 1;
+        }
+        //print("Created ruleset for id: {}, rules:\n", .{rule_id});
+        //for (ruleset[0..ri]) |rule| {
+        //    var cond = "ALL";
+        //    if (rule.condition) |c| {
+        //        cond = switch (c) {
+        //            Condition.LT => "LT<",
+        //            Condition.GT => "GT>",
+        //        };
+        //    }
+        //    print("{s} {} {} {}\n", .{ cond, rule.valueid, rule.limit, rule.destination });
+        //}
+        rulesets[rule_id] = ruleset;
+    }
+
+    const start = ruleid("in");
+
+    var total: usize = 0;
+    while (lines.next()) |line| {
+        var values: [4]u16 = undefined;
+        const end = indexOf(u8, line, '}').?;
+        var valueit = splitSca(u8, line[1..end], ',');
+        for (0..4) |i| {
+            const this = valueit.next().?;
+            values[i] = parseInt(u16, this[2..], 10) catch unreachable;
+        }
+        var current: u16 = start;
+        while (current > 1) {
+            const ruleset = rulesets[current];
+            for (ruleset) |rule| {
+                if (rule.condition) |cond| {
+                    const value = values[rule.valueid];
+                    const limit = rule.limit;
+                    const match: bool = switch (cond) {
+                        Condition.LT => (value < limit),
+                        Condition.GT => (value > limit),
+                    };
+                    if (match) {
+                        current = rule.destination;
+                        break;
+                    }
+                } else {
+                    current = rule.destination;
+                    break;
+                }
+            }
+        }
+        if (current == 0) {
+            for (values) |v| {
+                total += v;
+            }
+        }
+    }
+    return total;
+}
+
+test "day19_part2" {
+    const res = part2(testdata);
+    assert(res == 167409079868000);
+}
+
+const Range = struct {
+    min: u16,
+    max: u16,
+};
+
+const Rangeset = [4]Range;
+
+fn walkRule(rulesets: [65536]Ruleset, current: u16, inset: Rangeset) usize {
+    if (current == 0) {
+        var total: usize = 1;
+        for (inset) |range| {
+            total *= (range.max - range.min + 1);
+        }
+        return total;
+    }
+    if (current == 1) {
+        return 0;
+    }
+    var rangeset: Rangeset = inset;
+    var total: usize = 0;
+    for (rulesets[current]) |rule| {
+        if (rule.condition) |cond| {
+            const limit = rule.limit;
+            const vid: u2 = rule.valueid;
+            const value = rangeset[vid];
+            switch (cond) {
+                Condition.LT => {
+                    if (value.min < limit) {
+                        var newset: Rangeset = rangeset;
+                        newset[vid].max = @min(limit - 1, value.max);
+                        total += walkRule(rulesets, rule.destination, newset);
+                        const newmin = newset[vid].max + 1;
+                        if (newmin > rangeset[vid].max) {
+                            return total;
+                        }
+                        rangeset[vid].min = newmin;
+                    }
+                },
+                Condition.GT => {
+                    if (value.max > limit) {
+                        var newset: Rangeset = rangeset;
+                        newset[vid].min = @max(limit + 1, value.min);
+                        total += walkRule(rulesets, rule.destination, newset);
+                        const newmax = newset[vid].min - 1;
+                        if (newmax < rangeset[vid].min) {
+                            return total;
+                        }
+                        rangeset[vid].max = newmax;
+                    }
+                },
+            }
+        } else {
+            return total + walkRule(rulesets, rule.destination, rangeset);
+        }
+    }
+    unreachable;
+}
+
+fn part2(input: []const u8) usize {
+    var lines = splitSca(u8, input, '\n');
+    var rulesets: [65536]Ruleset = undefined;
+
+    while (lines.next()) |line| {
+        if (line.len < 2) break;
+        const start: usize = indexOf(u8, line, '{').?;
+        const end: usize = indexOf(u8, line, '}').?;
+        const rule_id = ruleid(line[0..start]);
+        var ruleit = splitSca(u8, line[start + 1 .. end], ',');
+        var ruleset: Ruleset = undefined;
+        var ri: u3 = 0;
+        while (ruleit.next()) |rulestr| {
+            const rule: Rule = switch (rulestr[0]) {
+                'A' => Rule{ .condition = null, .destination = 0 },
+                'R' => Rule{ .condition = null, .destination = 1 },
+                else => blk: {
+                    const colonpos = indexOf(u8, rulestr, ':');
+                    if (colonpos) |colon| {
+                        const value_id: u2 = valueid(rulestr[0]);
+                        const cond: Condition = switch (rulestr[1]) {
+                            '>' => Condition.GT,
+                            '<' => Condition.LT,
+                            else => unreachable,
+                        };
+                        const limit: u16 = parseInt(u16, rulestr[2..colon], 10) catch unreachable;
+                        const dest = ruleid(rulestr[colon + 1 .. rulestr.len]);
+                        break :blk Rule{ .condition = cond, .valueid = value_id, .limit = limit, .destination = dest };
+                    } else {
+                        const dest = ruleid(rulestr);
+                        break :blk Rule{ .condition = null, .destination = dest };
+                    }
+                },
+            };
+            ruleset[ri] = rule;
+            ri += 1;
+        }
+        //print("Created ruleset for id: {}, rules:\n", .{rule_id});
+        //for (ruleset[0..ri]) |rule| {
+        //    var cond = "ALL";
+        //    if (rule.condition) |c| {
+        //        cond = switch (c) {
+        //            Condition.LT => "LT<",
+        //            Condition.GT => "GT>",
+        //        };
+        //    }
+        //    print("{s} {} {} {}\n", .{ cond, rule.valueid, rule.limit, rule.destination });
+        //}
+        rulesets[rule_id] = ruleset;
+    }
+
+    var range: [4]Range = undefined;
+    for (0..4) |i| {
+        range[i] = .{ .min = 1, .max = 4000 };
+    }
+
+    const res = walkRule(rulesets, ruleid("in"), range);
+    return res;
+}
 
 pub fn main() !void {
-    
+    var timer = try std.time.Timer.start();
+    const res = part1(data);
+    const time = timer.lap();
+    const res2 = part2(data);
+    const time2 = timer.lap();
+    print("Part1: {}\n", .{res});
+    print("Part2: {}\n", .{res2});
+    print("Part1 took {}ns\n", .{time});
+    print("Part2 took {}ns\n", .{time2});
 }
 
 // Useful stdlib functions
